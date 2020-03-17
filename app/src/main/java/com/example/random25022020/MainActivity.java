13df9fe89d7481b8fc9c69938980a3c3a15e4fb1
kuvanpt2012 <kboylovemam@gmail.com>
2020-03-17 17:42:37 +0700

    lam tron

--- app/src/main/java/com/example/random25022020/MainActivity.java
+++ app/src/main/java/com/example/random25022020/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.random25022020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Math
+        //Lam tron
+        Double a  = 1.99;
+       // long a = Math.round(1.5);
+        // lam tron len
+        //double value1 = Math.ceil(a);
+        //lam tron xuong
+        double value2 = Math.floor(a);
+        Log.d("BBB",String.valueOf(value2));
     }
 }
